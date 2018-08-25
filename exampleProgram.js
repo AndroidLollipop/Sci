b75aca320abaf455ebda7758b39bc1b534f704b6
AndroidLollipop <liusiyongrd@gmail.com>
2018-08-25 20:31:17 +0800

    staging for arrays

--- exampleProgram.js
+++ exampleProgram.js
@@ -1,5 +1,5 @@
 var e = require("./simpleAndCleanInterfaceForRepl.js")
-var repl = e.getRepl()
+var repl = e.getRepl() // sadly no support for += yet, it isn't hard to implement but isn't a priority right now
 repl("num slowFibonacci(n){ \
     if (n < 3) { \
         return 1 \
@@ -7,10 +7,11 @@ repl("num slowFibonacci(n){ \
     return slowFibonacci(n-1)+slowFibonacci(n-2) \
 } \
 print(slowFibonacci(10)) \
-num fastFibonacci(n){num a = 1 \
+num fastFibonacci(n){ \
+    num a = 1 \
     num b = 1 \
     num c = 1 \
-    while (n>2) { \
+    while (n > 2) { \
         c = a+b \
         a = b \
         b = c \
@@ -22,7 +23,8 @@ print(fastFibonacci(10)) \
 num pi = 3.1415926535 \
 print(mathSin(pi/6)) \
 num closure(multiplier, start){ \
-    num add(){skye = skye + 1 \
+    num add(){ \
+        skye = skye + 1 \
         return skye*multiplier \
     } \
     num skye = start/multiplier-1 \
@@ -35,4 +37,8 @@ print(ver()) \
 print(clo()) \
 print(ver()) \
 print(clo()) \
-print(ver())")
+print(ver()) \
+print('language runtime written by AndroidLollipop in the year') \
+print(9*8*7*(6+5-4-3)+2*1) \
+print('example program written by AndroidLollipop in the year') \
+print((10+9)*(8+7+6)*5+4*3*2-1)")
