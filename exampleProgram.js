692f6c78db0e12f2d703d55056c42fd53bf4af56
AndroidLollipop <liusiyongrd@gmail.com>
2018-08-26 18:16:15 +0800

    added classic example

--- exampleProgram.js
+++ exampleProgram.js
@@ -1,13 +1,13 @@
 var e = require("./simpleAndCleanInterfaceForRepl.js")
 var repl = e.getRepl() // sadly no support for += yet, it isn't hard to implement but isn't a priority right now
-repl("num slowFibonacci(n){ \
+repl("num slowFibonacci(n) { \
     if (n < 3) { \
         return 1 \
     } \
     return slowFibonacci(n-1)+slowFibonacci(n-2) \
 } \
 print(slowFibonacci(10)) \
-num fastFibonacci(n){ \
+num fastFibonacci(n) { \
     num a = 1 \
     num b = 1 \
     num c = 1 \
@@ -21,8 +21,8 @@ num fastFibonacci(n){ \
 } \
 print(fastFibonacci(10)) \
 print(mathSin(mathPi/6)) \
-num closure(multiplier, start){ \
-    num add(){ \
+num closure(multiplier, start) { \
+    num add() { \
         skye = skye + 1 \
         return skye*multiplier \
     } \
@@ -43,4 +43,23 @@ print('example program written by AndroidLollipop in the year') \
 print((10+9)*(8+7+6)*5+4*3*2-1) \
 print(typeOf(1)) \
 print(typeOf(typeOf(1))) \
-print(typeOf(typeOf))")
+print(typeOf(typeOf)) \
+num church(n) { \
+    num a(f) { \
+        num b(g) { \
+            num i = 0 \
+            while (i < n) { \
+                g = f(g) \
+                i = i + 1 \
+            } \
+            return g \
+        } \
+        return b \
+    } \
+    return a \
+} \
+num inc(n) { \
+    return n+1 \
+} \
+print(church(5)(church(5))(inc)(1)) \
+print(closure(1, 100)())")
