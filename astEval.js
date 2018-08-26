0bc56aa9886f0f7d9082674b77f7313202d57ccc
AndroidLollipop <liusiyongrd@gmail.com>
2018-08-26 17:20:15 +0800

    removed unnecessary rebinding

--- astEval.js
+++ astEval.js
@@ -45,12 +45,12 @@ var adjoinScope = ([scopeGetter, scopeSetter, scopeDefiner]) => ([newScopeGetter
         return newScopeSetter(name, value)
     }, newScopeDefiner])
 } // i imagine this is how scope is implemented in javascript, and that explains why setting an undeclared variable makes it global
-var defineInScope = ([parentScopeGetter, parentScopeSetter, parentScopeDefiner]) => (identifiers) => ([scopeGetter, scopeSetter, scopeDefiner]) => (expressions) => {
+var defineInScope = (sco) => (identifiers) => ([scopeGetter, scopeSetter, scopeDefiner]) => (expressions) => {
     for (var i = 0; i < identifiers.children.length; i++) {
         if (expressions.children[i] == undefined) {
             return
         }
-        scopeDefiner(identifiers.children[i].canonicalString, evaluateExpression([parentScopeGetter, parentScopeSetter, parentScopeDefiner])(expressions.children[i]))
+        scopeDefiner(identifiers.children[i].canonicalString, sco(expressions.children[i]))
     }
 }
 var collapseString = (nodeChildren) => {
@@ -58,9 +58,9 @@ var collapseString = (nodeChildren) => {
     nodeChildren.map(x => x.type == "alphanumeric literal" ? ret += x.canonicalString: x.canonicalString == "\\" ? ret += "\\" : x.canonicalString == "n" ? ret += "\n" : ret += x.canonicalString)
     return ret
 }
-var evaluateCondition = ([scopeGetter, scopeSetter, scopeDefiner]) => ([L, C, R]) => {
-    L = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(L)
-    R = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(R)
+var evaluateCondition = (sco) => ([L, C, R]) => {
+    L = sco(L)
+    R = sco(R)
     C = C.canonicalString
     if (L.type !== "number" || R.type !== "number") {
         return -1
@@ -78,120 +78,123 @@ var evaluateCondition = ([scopeGetter, scopeSetter, scopeDefiner]) => ([L, C, R]
     }
     return 0
 }
-var evaluateExpression = ([scopeGetter, scopeSetter, scopeDefiner]) => (expression) => {
-    if (expression.type == "function declaration") {
-        scopeDefiner(expression.children.filter((x) => x.type == "identifier")[0].canonicalString, { type: "function", parentScope: [scopeGetter, scopeSetter, scopeDefiner], parameters: expression.children.filter((x) => x.type == "parameter declaration")[0], body: expression.children.filter((x) => x.type == "function body")[0]})
-        return scopeGetter(expression.children.filter((x) => x.type == "identifier")[0].canonicalString)
-    }
-    else if (expression.type == "!!!BUILTIN") {
-        var res = expression.builtin([scopeGetter, scopeSetter, scopeDefiner])
-        return res
-    }
-    else if (expression.type == "if expression" || expression.type == "if else expression") {
-        var res = evaluateCondition([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0].children)
-        if (res == 1) {
-            return evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[1])
+var evaluateExpression = ([scopeGetter, scopeSetter, scopeDefiner]) => {
+    var sco = (expression) => {
+        if (expression.type == "function declaration") {
+            scopeDefiner(expression.children.filter((x) => x.type == "identifier")[0].canonicalString, { type: "function", parentScope: [scopeGetter, scopeSetter, scopeDefiner], parameters: expression.children.filter((x) => x.type == "parameter declaration")[0], body: expression.children.filter((x) => x.type == "function body")[0]})
+            return scopeGetter(expression.children.filter((x) => x.type == "identifier")[0].canonicalString)
         }
-        else if (res == 0 && expression.type == "if else expression") {
-            return evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[2])
+        else if (expression.type == "!!!BUILTIN") {
+            var res = expression.builtin([scopeGetter, scopeSetter, scopeDefiner])
+            return res
+        }
+        else if (expression.type == "if expression" || expression.type == "if else expression") {
+            var res = evaluateCondition(sco)(expression.children[0].children)
+            if (res == 1) {
+                return sco(expression.children[1])
+            }
+            else if (res == 0 && expression.type == "if else expression") {
+                return sco(expression.children[2])
+            }
+            return { type: "void" }
+        }
+        else if (expression.type == "while expression") {
+            var res = evaluateCondition(sco)(expression.children[0].children)
+            var ret = { type: "void" }
+            while (res == 1) {
+                ret = sco(expression.children[1])
+                if (ret.type == "!!!INTERNAL INTERPRETER CONTROL") {
+                    return ret
+                }
+                res = evaluateCondition(sco)(expression.children[0].children)
+            }
+            return ret
+        }
+        else if (expression.type == "function call") {
+            var target = sco(expression.children[0])
+            var targetScope = adjoinScope(target.parentScope)(emptyScope())
+            defineInScope(sco)(target.parameters)(targetScope)(expression.children.filter((x) => x.type == "function call bindings")[0])
+            var expRes = evaluateExpression(targetScope)(target.body)
+            if (expRes.type !== "!!!INTERNAL INTERPRETER CONTROL" || expRes.control !== "return") { // someone is trying to trick us
+                return { type: "void" }
+            }
+            return expRes.value
+        }
+        else if (expression.type == "function body") {
+            var expRes = { type: "void" }
+            for (var i = 0; i < expression.children.length; i++) {
+                expRes = sco(expression.children[i])
+                if (expRes.type == "!!!INTERNAL INTERPRETER CONTROL") {
+                    return expRes
+                }
+            }
+            return { type: "!!!INTERNAL INTERPRETER CONTROL", control: "return", value: expRes } // functions implicitly return, only {} returns void
+        }
+        else if (expression.type == "block body") {
+            var expRes = { type: "void" }
+            for (var i = 0; i < expression.children.length; i++) {
+                expRes = sco(expression.children[i])
+                if (expRes.type == "!!!INTERNAL INTERPRETER CONTROL") {
+                    return expRes
+                }
+            }
+            return expRes
+        }
+        else if (expression.type == "return statement") {
+            var expRes = sco(expression.children[0])
+            return { type: "!!!INTERNAL INTERPRETER CONTROL", control: "return", value: expRes } // this could be a source of vulnerabilities, damn
+        }
+        else if (expression.type == "variable declaration") {
+            var expRes = sco(expression.children[3])
+            scopeDefiner(expression.children.filter((x) => x.type == "identifier")[0].canonicalString, expRes)
+            return expRes
+        }
+        else if (expression.type == "variable set") {
+            var expRes = sco(expression.children[2])
+            scopeSetter(expression.children.filter((x) => x.type == "identifier")[0].canonicalString, expRes)
+            return expRes
+        }
+        else if (expression.type == "parenthesized expression") {
+            if (expression.children[0] == undefined) {
+                return { type: "void" }
+            }
+            return unwrap(sco(expression.children[0]))
+        }
+        else if (expression.type == "expression") {
+            if (expression.children[0] == undefined) {
+                return { type: "void" }
+            }
+            var acc = unwrap(sco(expression.children[0]))
+            for (var i = 1; i < expression.children.length; i+=2) {
+                acc = operate(acc, expression.children[i], unwrap(sco(expression.children[i+1])))
+            }
+            return acc
+        }
+        else if (expression.type == "negated literal") {
+            if (expression.children[0] == undefined) {
+                return { type: "void" }
+            }
+            var expRes = sco(expression.children[0])
+            if (expRes.type !== "number") {
+                return { type: "void" }
+            }
+            return { type: "number", value: -expRes.value }
+        }
+        else if (expression.type == "float literal") {
+            return { type: "number", value: parseFloat(expression.canonicalString) } // technically i'm not supposed to do this
+        }
+        else if (expression.type == "integral literal") {
+            return { type: "number", value: parseInt(expression.canonicalString) }
+        }
+        else if (expression.type == "string literal") {
+            return { type: "string", value: collapseString(expression.children) }
+        }
+        else if (expression.type == "identifier") {
+            return scopeGetter(expression.canonicalString)
         }
         return { type: "void" }
     }
-    else if (expression.type == "while expression") {
-        var res = evaluateCondition([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0].children)
-        var ret = { type: "void" }
-        while (res == 1) {
-            ret = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[1])
-            if (ret.type == "!!!INTERNAL INTERPRETER CONTROL") {
-                return ret
-            }
-            res = evaluateCondition([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0].children)
-        }
-        return ret
-    }
-    else if (expression.type == "function call") {
-        var target = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0])
-        var targetScope = adjoinScope(target.parentScope)(emptyScope())
-        defineInScope([scopeGetter, scopeSetter, scopeDefiner])(target.parameters)(targetScope)(expression.children.filter((x) => x.type == "function call bindings")[0])
-        var expRes = evaluateExpression(targetScope)(target.body)
-        if (expRes.type !== "!!!INTERNAL INTERPRETER CONTROL" || expRes.control !== "return") { // someone is trying to trick us
-            return { type: "void" }
-        }
-        return expRes.value
-    }
-    else if (expression.type == "function body") {
-        var expRes = { type: "void" }
-        for (var i = 0; i < expression.children.length; i++) {
-            expRes = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[i])
-            if (expRes.type == "!!!INTERNAL INTERPRETER CONTROL") {
-                return expRes
-            }
-        }
-        return { type: "!!!INTERNAL INTERPRETER CONTROL", control: "return", value: expRes } // functions implicitly return, only {} returns void
-    }
-    else if (expression.type == "block body") {
-        var expRes = { type: "void" }
-        for (var i = 0; i < expression.children.length; i++) {
-            expRes = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[i])
-            if (expRes.type == "!!!INTERNAL INTERPRETER CONTROL") {
-                return expRes
-            }
-        }
-        return expRes
-    }
-    else if (expression.type == "return statement") {
-        var expRes = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0])
-        return { type: "!!!INTERNAL INTERPRETER CONTROL", control: "return", value: expRes } // this could be a source of vulnerabilities, damn
-    }
-    else if (expression.type == "variable declaration") {
-        var expRes = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[3])
-        scopeDefiner(expression.children.filter((x) => x.type == "identifier")[0].canonicalString, expRes)
-        return expRes
-    }
-    else if (expression.type == "variable set") {
-        var expRes = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[2])
-        scopeSetter(expression.children.filter((x) => x.type == "identifier")[0].canonicalString, expRes)
-        return expRes
-    }
-    else if (expression.type == "parenthesized expression") {
-        if (expression.children[0] == undefined) {
-            return { type: "void" }
-        }
-        return unwrap(evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0]))
-    }
-    else if (expression.type == "expression") {
-        if (expression.children[0] == undefined) {
-            return { type: "void" }
-        }
-        var acc = unwrap(evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0]))
-        for (var i = 1; i < expression.children.length; i+=2) {
-            acc = operate(acc, expression.children[i], unwrap(evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[i+1])))
-        }
-        return acc
-    }
-    else if (expression.type == "negated literal") {
-        if (expression.children[0] == undefined) {
-            return { type: "void" }
-        }
-        var expRes = evaluateExpression([scopeGetter, scopeSetter, scopeDefiner])(expression.children[0])
-        if (expRes.type !== "number") {
-            return { type: "void" }
-        }
-        return { type: "number", value: -expRes.value }
-    }
-    else if (expression.type == "float literal") {
-        return { type: "number", value: parseFloat(expression.canonicalString) } // technically i'm not supposed to do this
-    }
-    else if (expression.type == "integral literal") {
-        return { type: "number", value: parseInt(expression.canonicalString) }
-    }
-    else if (expression.type == "string literal") {
-        return { type: "string", value: collapseString(expression.children) }
-    }
-    else if (expression.type == "identifier") {
-        return scopeGetter(expression.canonicalString)
-    }
-    return { type: "void" }
+    return sco
 }
 module.exports = {
     evaluateExpression: evaluateExpression,
