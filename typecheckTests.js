6f722d77dd260bb9c60ee38bde08798b6071851a
AndroidLollipop <liusiyongrd@gmail.com>
2018-08-27 02:51:22 +0800

    improved typecheck tests

--- typecheckTests.js
+++ typecheckTests.js
@@ -1,22 +1,26 @@
 var e = require("./simpleAndCleanInterfaceForRepl.js")
 var repl = e.getRepl() // sadly no support for += yet, it isn't hard to implement but isn't a priority right now
-try{
+var passed = 1
+try {
     repl("var inc(x) { \
         return x+1 \
     } \
     str k = inc(1)") // this should fail since return type of inc is num
+    passed = 0
 }
 catch(e) {
     console.log(e)
 }
-try{
+try {
     repl("num k = ' '") // this should fail since type of ' ' is str
+    passed = 0
 }
 catch(e) {
     console.log(e)
 }
 try {
     repl("str k = 5") // this should fail since type of 5 is num
+    passed = 0
 }
 catch(e) {
     console.log(e)
@@ -26,14 +30,31 @@ try {
         return x+1 \
     } \
     inc(1)") // this should fail since return type of inc is num
+    passed = 0
 }
 catch(e) {
     console.log(e)
 }
 try {
-    repl("arr = [1, 2] \
+    repl("var arr = [1, 2] \
     str k = arr[1]") // this should fail since type of arr[1] is num
+    passed = 0
 }
 catch(e) {
     console.log(e)
+}
+try {
+    repl("var arr = [1, 2] \
+    str k = ' ' \
+    k = arr[1]") // this should fail since type of arr[1] is num
+    passed = 0
+}
+catch(e) {
+    console.log(e)
+}
+if (passed == 1) {
+    console.log("typecheck test PASSED!")
+}
+else {
+    console.log("typecheck test FAILED!")
 }
